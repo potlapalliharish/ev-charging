3b9499b7a66074ac1a76d8f1dac0ea258dfdb05a
yash7780 <yaswanthnarava@gmail.com>
2023-04-28 20:13:42 +0530

    active tab

--- src/Host/HostDashboard.js
+++ src/Host/HostDashboard.js
@@ -4,10 +4,16 @@ import { Link } from 'react-router-dom';
 import HostCard from './HostCard';
 import { FiPlus, Fi } from 'react-icons/fi';
 import { RiLogoutBoxLine } from 'react-icons/ri';
+import { useState } from 'react';
 function HostDashboard() {
   const navigate = useNavigate()
   const handleBack = () => {
     navigate('/');
+  };
+  const [activeTab, setActiveTab] = useState('slots');
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
   };
   const cards = [
     {
@@ -30,18 +36,18 @@ function HostDashboard() {
   return (
     <div className="dashboard">
       <div>
-      <nav className="dashboard-nav">
-        <Link to="/"><img src="https://cdn-icons-png.flaticon.com/512/2991/2991201.png" /></Link>
-        <div className="dashboard-tabs">
-          <button>
-            Slots
-          </button>
-          <button>
-            Requests
-          </button>
-        </div>
-        <Link to="/"><RiLogoutBoxLine size={25} color="#fff" /></Link>
-      </nav>
+        <nav className="dashboard-nav">
+          <Link to="/"><img src="https://cdn-icons-png.flaticon.com/512/2991/2991201.png" /></Link>
+          <div className="dashboard-tabs">
+            <button className={activeTab === 'slots' ? 'active' : ''} onClick={() => handleTabClick('slots')}>
+              Slots
+            </button>
+            <button className={activeTab === 'requests' ? 'active' : ''} onClick={() => handleTabClick('requests')}>
+              Requests
+            </button>
+          </div>
+          <Link to="/"><RiLogoutBoxLine size={25} color="#fff" /></Link>
+        </nav>
       </div>
       <div className="plus-icon"><button>
         <FiPlus />
