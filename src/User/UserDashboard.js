1c71e73342975fa9bd1adf936ec67976cf115b35
harish potlapali <harish.potlapalli@hexagon.com>
2023-04-28 19:08:53 +0530

    Add Images for Approved and complete

--- src/User/UserDashboard.js
+++ src/User/UserDashboard.js
@@ -1,9 +1,9 @@
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import Card from './Card';
 import { RiLogoutBoxLine } from 'react-icons/ri';
-import * as SlotsService  from '../Services/SlotsService';
+import * as SlotsService from '../Services/SlotsService';
 import * as RequestsService from '../Services/RequestsService';
 
 function UserDashboard() {
@@ -14,58 +14,60 @@ function UserDashboard() {
     };
     const requests = RequestsService.readAllItems();
     const cards = SlotsService.readAllActiveItems().filter(c => !requests.find(r => r.name === c.name));
-    
+
     const [activeTab, setActiveTab] = useState('available');
 
     const handleTabClick = (tab) => {
         setActiveTab(tab);
-      };
-      const onRequested = (name) => {
+    };
+    const onRequested = (name) => {
         RequestsService.createItem({
             ...cards.find(c => c.name === name),
-            time: new Date()
+            time: new Date(),
+            isApproved: false,
+            isCompleted: false
         })
         //hack to force rerender a component
-        setCount(count+1);
-      };
-      const onCanceled = (name) => {
+        setCount(count + 1);
+    };
+    const onCanceled = (name) => {
         RequestsService.deleteItem(name)
         //hack to force rerender a component
-        setCount(count+1);
-      };
-     
-      const getLocationLink =(card) =>{
+        setCount(count + 1);
+    };
+
+    const getLocationLink = (card) => {
         return `https://www.google.com/maps/dir/?api=1&origin=My+Location&destination=${card.latitude},${card.longitude}`;
-      };
-  return (
-    <div className="dashboard">
-    <nav className="dashboard-nav">
-      <Link to="/"><img src="https://cdn-icons-png.flaticon.com/512/2991/2991201.png" /></Link>
-      <div className="dashboard-tabs">
-          <button
-            className={activeTab === 'available' ? 'active' : ''}
-            onClick={() => handleTabClick('available')}
-          >
-            Available
-          </button>
-          <button
-            className={activeTab === 'requested' ? 'active' : ''}
-            onClick={() => handleTabClick('requested')}
-          >
-            Requested
-          </button>
+    };
+    return (
+        <div className="dashboard">
+            <nav className="dashboard-nav">
+                <Link to="/"><img src="https://cdn-icons-png.flaticon.com/512/2991/2991201.png" /></Link>
+                <div className="dashboard-tabs">
+                    <button
+                        className={activeTab === 'available' ? 'active' : ''}
+                        onClick={() => handleTabClick('available')}
+                    >
+                        Available
+                    </button>
+                    <button
+                        className={activeTab === 'requested' ? 'active' : ''}
+                        onClick={() => handleTabClick('requested')}
+                    >
+                        Requested
+                    </button>
+                </div>
+                <Link to="/"><RiLogoutBoxLine size={25} color="#fff" /></Link>
+            </nav>
+
+            <div className="cards-container">
+                {activeTab === 'available' &&
+                    cards.map((card, index) => <Card key={index} {...card} link={getLocationLink(card)} requested={false} onRequested={onRequested} />)}
+                {activeTab === 'requested' &&
+                    requests.map((card, index) => <Card key={index} {...card} link={getLocationLink(card)} requested={true} onCanceled={onCanceled} />)}
+            </div>
         </div>
-      <Link to="/"><RiLogoutBoxLine size={25} color="#fff" /></Link>
-    </nav>
-    
-    <div className="cards-container">
-        {activeTab === 'available' &&
-            cards.map((card, index) => <Card key={index} {...card} link={getLocationLink(card)} requested={false} onRequested={onRequested}/>)}
-        {activeTab === 'requested' && 
-            requests.map((card, index) => <Card key={index} {...card} link={getLocationLink(card)} requested={true} onCanceled={onCanceled}/>)}
-      </div>
-  </div>
-  );
+    );
 }
 
 export default UserDashboard;
